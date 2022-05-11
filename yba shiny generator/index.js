abcc102f280b53109804a0905346229a5f5242c7
mr cool <edward.peter.cole@gmail.com>
2022-05-11 15:04:12 -0700

    Add files via upload

---
+++ yba shiny generator/index.js
@@ -0,0 +1,25 @@
+let count = document.querySelector("#count")
+let button = document.querySelector("#button")
+let pity = document.querySelector("#chance")
+
+let rolls = 0
+let chance = 100
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+button.onclick = function() {
+    let shiny = false
+    rolls = 0
+    chance = 100
+    while(!shiny) {
+        let rand = getRandomInt(10000)
+        if(rand <= chance) {
+            shiny = true
+        } else {
+            chance += 2
+        }
+        rolls += 1
+        count.innerHTML = "Amount of rolls: "+rolls
+        pity.innerHTML = "pity: "+chance/100+"%"
+    }
+}
